a99f09b0d760c9a186bf99744801347466def808
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-12-29 14:27:17 +0300

    Initial commit.

---
+++ BufferArray.Tests/BufferDataTests.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace BufferArray.Tests
+{
+    [TestFixture]
+    public class BufferDataTests
+    {
+        [TestCase(4)]
+        [TestCase(16)]
+        [TestCase(256)]
+        [TestCase(512)]
+        public void ToArray_OnBaseEnumerable_(int count)
+        {
+            var actual = Enumerable.Range(1, count).ToArray();
+            
+            var (expected, length) = BufferData.ToArray(Enumerable.Range(1, count));
+            
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(count, length);
+        }
+        
+        [TestCase(4_000)]
+        [TestCase(10_000)]
+        public void ToArray_OnBaseEnumerable(int count)
+        {
+            var actual = Enumerable.Range(1, count).ToArray();
+            
+            var (expected, length) = BufferData.ToArray(Enumerable.Range(1, count));
+            
+            Assert.AreNotEqual(expected, actual);
+            Assert.AreEqual(count, length);
+        }
+        
+        [TestCase(4)]
+        [TestCase(16)]
+        [TestCase(256)]
+        [TestCase(512)]
+        [TestCase(4_000)]
+        [TestCase(10_000)]
+        public void ToArray_OnBaseCollection(int count)
+        {
+            var actual = Enumerable.Range(1, count).ToArray();
+            
+            var (expected, length) = BufferData.ToArray(actual);
+            
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(count, length);
+        }
+        
+        [Test]
+        public void ToArray_ReturnEmptyArray()
+        {
+            var (expected, count) = BufferData.ToArray(Enumerable.Repeat(12,0));
+            
+            Assert.AreEqual(expected, Array.Empty<int>());
+            Assert.AreEqual(count, 0);
+        }
+    }
+}
---
+++ BufferArray/BufferData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("BufferArray.Tests")]
+
+namespace BufferArray
+{
+    /// <summary>
+    /// Implements array helper methods.
+    /// </summary>
+    internal static class BufferData
+    {
+        /// <summary>
+        /// Creates array on base of enumerable sequence.
+        /// </summary>
+        /// <param name="source">The enumerable sequence.</param>
+        /// <typeparam name="T">Type of the elements of the sequence.</typeparam>
+        /// <returns>Single dimension zero based array.</returns>
+        internal static (T[] buffer, int count) ToArray<T>(IEnumerable<T> source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
